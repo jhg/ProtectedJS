4184f717512358f3853e7b4d7170909e907d27a2
Jesús Hernández Gormaz <0.jesus.hg@gmail.com>
2019-11-16 11:17:03 +0100

    Use compression level 9

--- src/encryptJsFile.js
+++ src/encryptJsFile.js
@@ -6,7 +6,7 @@ const crypto = require('crypto');
 module.exports = function(jsPath, pjsPath, password){
   // Load source JS
   let src = fs.readFileSync(jsPath, {encoding: 'utf8'});
-  src = zlib.gzipSync(Buffer.from(src, 'utf8'));
+  src = zlib.gzipSync(Buffer.from(src, 'utf8'), {level: 9});
   // Encrypt source JS
   const cipher = crypto.createCipher('aes-256-ctr', password);
   let protectedJS = cipher.update(src, 'binary', 'base64');
