86afde6333140100d79e2f80c2877c92c0f44b0a
Jesús Hernández Gormaz <0.jesus.hg@gmail.com>
2019-11-16 10:14:24 +0100

    Refactor of test and show sizes

--- test/test.js
+++ test/test.js
@@ -7,14 +7,17 @@ const pjs = require('..');
 describe('PJS', function(){
   before(function(){
     mockfs({
-      /* empty directory */
+      "dummy.js": "module.exports = true;"
     });
+    let dummyJsSize = fs.statSync("dummy.js")["size"];
+    console.log(`dummy JS ${dummyJsSize} bytes`);
   });
   after(function(){
+    let dummyPjsSize = fs.statSync("dummy.pjs")["size"];
+    console.log(`dummy PJS ${dummyPjsSize} bytes`);
     mockfs.restore();
   });
   it('should encrypt a JS file and import created PJS file', function(){
-    fs.writeFileSync('dummy.js', 'module.exports = true;');
     pjs.encryptJsFile('dummy.js', 'dummy.pjs', 'dummy');
     let dummy = pjs.importPjsFile('dummy.pjs', 'dummy');
     assert.equal(dummy, true);
