5ad2963f33287bb5e61c181456cdd52b357b2052
Jesús Hernández Gormaz <0.jesus.hg@gmail.com>
2019-11-16 11:21:31 +0100

    Remove comments from JS file

--- src/encryptJsFile.js
+++ src/encryptJsFile.js
@@ -6,6 +6,8 @@ const crypto = require('crypto');
 module.exports = function(jsPath, pjsPath, password){
   // Load source JS
   let src = fs.readFileSync(jsPath, {encoding: 'utf8'});
+  // Remove comments and compress
+  src = src.replace(/(\/\*[^*]*\*\/)|(\/\/[^*]*)/g, '');
   src = zlib.gzipSync(Buffer.from(src, 'utf8'), {level: 9});
   // Encrypt source JS
   const cipher = crypto.createCipher('aes-256-ctr', password);
