c9dfc53390b79e58a15d090997c445cbdb0027da
Jesús Hernández Gormaz <0.jesus.hg@gmail.com>
2019-11-16 10:58:24 +0100

    Compress JS with Gzip before to encrypt

--- src/encryptJsFile.js
+++ src/encryptJsFile.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
+const zlib = require('zlib');
 const crypto = require('crypto');
 
 
 module.exports = function(jsPath, pjsPath, password){
   // Load source JS
   let src = fs.readFileSync(jsPath, {encoding: 'utf8'});
+  src = zlib.gzipSync(Buffer.from(src, 'utf8'));
   // Encrypt source JS
   const cipher = crypto.createCipher('aes-256-ctr', password);
-  let protectedJS = cipher.update(src, 'utf8', 'base64');
+  let protectedJS = cipher.update(src, 'binary', 'base64');
   protectedJS += cipher.final('base64');
   // Write ProtectedJS
   fs.writeFileSync(pjsPath, protectedJS, {encoding: 'utf8'});
--- src/importPjsFile.js
+++ src/importPjsFile.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const zlib = require('zlib');
 const crypto = require('crypto');
 
 
@@ -8,8 +9,9 @@ module.exports = function(pjsPath, password){
   let protectedJS = fs.readFileSync(pjsPath, {encoding: 'utf8'});
   // Decrypt ProtectedJS
   const decipher = crypto.createDecipher('aes-256-ctr', password);
-  let src = decipher.update(protectedJS, 'base64', 'utf8');
-  src += decipher.final('utf8');
+  let src = decipher.update(protectedJS, 'base64', 'binary');
+  src += decipher.final('binary');
+  src = zlib.gunzipSync(Buffer.from(src, 'binary')).toString('utf8');
   // Create new module from decrypted JS
   const Module = module.constructor;
   let dynModule = new Module();
